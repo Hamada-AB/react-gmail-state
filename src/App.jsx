dab3642400915b9175f75f389ef79299459e42d8
Hamada Abdelaal <hamada_abdelaal@hotmail.com>
2024-04-25 07:43:04 +0200

    gmail state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,59 @@
-import Header from './components/Header'
-import initialEmails from './data/emails'
+import Header from "./components/Header";
+import initialEmails from "./data/emails";
+import { useState } from "react";
 
-import './styles/App.css'
+import "./styles/App.css";
 
 function App() {
   // Use initialEmails for state
-  console.log(initialEmails)
+  const [emailsList, setEmailsList] = useState(initialEmails);
+  const [inbox, setInbox] = useState(getUnreadMails(emailsList));
+  const [starred, setStarred] = useState(getStarredMails(emailsList));
+
+  function getUnreadMails(array) {
+    return array.filter((email) => !email.read);
+  }
+
+  function getStarredMails(array) {
+    return array.filter((email) => email.starred);
+  }
+
+  function handleSelectClick(event) {
+    const { id } = event.target;
+
+    const selectedMails = emailsList.map((email) => {
+      if (email.id == id) {
+        !email.read ? (email.read = true) : (email.read = false);
+      }
+      return email;
+    });
+
+    setEmailsList(selectedMails);
+    setInbox(getUnreadMails(selectedMails));
+  }
+
+  function handleStarredClick(event) {
+    const { id } = event.target;
+
+    const starredMails = emailsList.map((email) => {
+      if (email.id == id) {
+        !email.starred ? (email.starred = true) : (email.starred = false);
+      }
+      return email;
+    });
+
+    setStarred(getStarredMails(starredMails));
+  }
+
+  function handleHideReadChange(event) {
+    const { checked } = event.target;
+
+    if (checked) {
+      setEmailsList(inbox);
+    } else {
+      setEmailsList(initialEmails);
+    }
+  }
 
   return (
     <div className="app">
@@ -14,33 +62,69 @@ function App() {
         <ul className="inbox-list">
           <li
             className="item active"
-            // onClick={() => {}}
+            onClick={() => setEmailsList(initialEmails)}
           >
             <span className="label">Inbox</span>
-            <span className="count">?</span>
+            <span className="count">{inbox.length}</span>
           </li>
-          <li
-            className="item"
-            // onClick={() => {}}
-          >
+
+          <li className="item" onClick={() => setEmailsList(starred)}>
             <span className="label">Starred</span>
-            <span className="count">?</span>
+            <span className="count">{starred.length}</span>
           </li>
 
           <li className="item toggle">
-            <label for="hide-read">Hide read</label>
+            <label htmlFor="hide-read">Hide read</label>
             <input
               id="hide-read"
               type="checkbox"
-              checked={false}
-              // onChange={() => {}}
+              disabled={inbox.length === 0 ? true : false}
+              onChange={handleHideReadChange}
             />
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">
+        {/* Render a list of emails here */}
+        {emailsList.map((email) => {
+          let liClasses;
+          if (!email.read) {
+            liClasses = "email unread";
+          } else {
+            liClasses = "email read";
+          }
+
+          return (
+            <li className={liClasses} key={email.id}>
+              <div className="select">
+                <input
+                  className="select-checkbox"
+                  type="checkbox"
+                  id={email.id}
+                  checked={email.read}
+                  onChange={handleSelectClick}
+                />
+              </div>
+
+              <div className="star">
+                <input
+                  className="star-checkbox"
+                  type="checkbox"
+                  id={email.id}
+                  checked={email.starred}
+                  onChange={handleStarredClick}
+                />
+              </div>
+
+              <div className="sender">{email.sender}</div>
+
+              <div className="title">{email.title}</div>
+            </li>
+          );
+        })}
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/data/emails.js
+++ src/data/emails.js
@@ -4,34 +4,34 @@ export default [
     sender: `Zoom`,
     title: `Cloud Recording - Nicolas Marcora's Personal Meeting Room is now available`,
     starred: false,
-    read: true
+    read: false,
   },
   {
     id: 2,
     sender: `Zoom`,
     title: `Sean Davison has joined your Personal Meeting Room`,
-    starred: false,
-    read: false
+    starred: true,
+    read: true,
   },
   {
     id: 3,
     sender: `Notion`,
     title: `1 update in Boolean`,
-    starred: true,
-    read: true
+    starred: false,
+    read: true,
   },
   {
     id: 4,
     sender: `The Calendly Team`,
     title: `Use more than one calendar?`,
-    starred: false,
-    read: false
+    starred: true,
+    read: false,
   },
   {
     id: 5,
     sender: `Patrick`,
     title: `Updated invitation: Coding chat with Nico`,
-    starred: true,
-    read: false
-  }
-]
+    starred: false,
+    read: false,
+  },
+];
